eced6f43e50aec1f03ceb3b241035fa07ea93d0b
eyalganor123 <eyalganor@gmail.com>
2021-09-14 23:56:10 +0300

    more css.. and browser support

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import "./Buttons.css"
 
-export default function Buttons( {handleSpeak,handleNext, browser}) {
-  
-  if(browser){
-  return (
- 
-    <div>
-           <button onClick={handleSpeak}>speak</button>
-        <button onClick={handleNext}>next</button>
-    </div>
-  )}else {
+export default function Buttons({ handleSpeak, handleNext, browser }) {
+
+  if (browser) {
     return (
-      <div></div>
+      <div>
+        <button onClick={handleSpeak}>speak</button>
+        <button onClick={handleNext}>next</button>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <button onClick={handleNext}>next</button>
+      </div>
     )
   }
 }
